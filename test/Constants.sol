cd81f7800005bbddddbca8f8e5e00a92fcf1739b
dan2xxx <dan2xxx@gmail.com>
2025-03-27 13:46:37 +0200

    Fix the test for USDE token

--- test/Constants.sol
+++ test/Constants.sol
@@ -7,8 +7,9 @@ contract Constants {
     uint256 public constant MIN_PRICE = 1e18; // Minimum EUI/USDE price
     bytes32 public constant DEFAULT_ADMIN_ROLE = 0x00;
     bytes32 public constant MINT_ROLE = keccak256("MINT_ROLE");
+    bytes32 public constant INVEST_TOKEN_ROLE = keccak256("INVEST_TOKEN_ROLE");
     bytes32 public constant BURN_ROLE = keccak256("BURN_ROLE");
-    bytes32 public constant PAUSE_ROLE = keccak256("PAUSE_ROLE");
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant FREEZE_ROLE = keccak256("FREEZE_ROLE");
     bytes32 public constant BLOCK_ROLE = keccak256("BLOCK_ROLE");
     bytes32 public constant ALLOW_ROLE = keccak256("ALLOW_ROLE");
